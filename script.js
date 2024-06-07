f5a809967c1d4b64a05137eea25da30127b53220
unknown <labapawel@gmail.com>
2024-06-07 19:02:33 +0200

    dodano opcje odsłony pol

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const XIlosc = 20; // wielkość pola
 const YIlosc = 20; // wielkość pola
-const min = 20; // liczba min
+const min = 40; // liczba min
 const $ = (n) => document.querySelector(n);
 const offsetMap = [[-1,-1], [-1,0], [-1,1], [0,-1], [0,1], [1,-1], [1,0], [1,1]];
 let matryca=[]
@@ -50,6 +50,56 @@ function wyswielt(){
     })
 }
 
+function odslonaMin(){
+    matryca.forEach(y=>{
+        y.forEach(e=>{
+            if(e.mina && !e.oznaczone){
+                e.pole.classList.remove('hide');
+                e.pole.classList.add('mina');
+            }
+        })
+    })
+}
+
+function odslona(x, y){
+
+    let m = matryca[y][x];
+    console.log(m);
+    if(m.oznaczone){
+        return;
+    }
+    if(m.mina)
+        {
+            odslonaMin();
+            return false;
+        }
+    else 
+    if(m.ileMin==0)
+    {
+        m.odslona = true;
+        m.pole.classList.remove('hide');
+        
+        offsetMap.forEach(off=>{
+            let px=off[1]+x;
+            let py=off[0]+y;
+            if( px<XIlosc && px>=0 && py<YIlosc && py >= 0 ){
+            //    console.log(py, px);
+                m = matryca[py][px];
+                if(m.ileMin==0 && !m.odslona)
+                    odslona(px, py);
+                else 
+                {
+                    m.pole.classList.add("mina"+m.ileMin);
+                    m.pole.classList.remove('hide');
+                }       
+            }
+        
+        })
+   } else {
+     m.pole.classList.add("mina"+m.ileMin);
+     m.pole.classList.remove('hide');
+   }
+}
 
 function init() {
   console.log("---------- init -----------");
@@ -64,6 +114,11 @@ function init() {
         let div = document.createElement('div');
         div.className = 'poleminy hide';
         //div.innerHTML = "0";
+        div.addEventListener('click', (e)=>{
+          let x = parseInt(e.target.getAttribute('x')); 
+          let y = parseInt(e.target.getAttribute('y'));
+          odslona(x, y);
+        })
         div.addEventListener('contextmenu', (e)=>{
             e.preventDefault();
             let pole = matryca[e.target.getAttribute('y')][e.target.getAttribute('x')];
@@ -83,7 +138,8 @@ function init() {
         }, false);
         div.setAttribute('x', x);
         div.setAttribute('y', y);
-        let poleMiny = {mina:false, ileMin:0, pole:div, oznaczone:false}
+        
+        let poleMiny = {mina:false, ileMin:0, pole:div, oznaczone:false, odsloniete: false}
         matryca[y].push(poleMiny);
         k.append(div);
     }
